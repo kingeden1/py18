8f8ece4cfcaa262f23c23c89acfccb82b5ddd479
Kingeden1 <stewjf@yahoo.com>
2025-04-03 21:01:56 +0300

    py18

---
+++ Rock,paper,scissors.py
@@ -0,0 +1,21 @@
+import random
+
+option = ["Rock","Paper","Scissors"]
+
+user_choice = input("Chosse Rock,Paper, or Scissos: ")
+
+computer_choice = random.choice(option)
+
+print("You chose:", user_choice)
+print("computer chose:", computer_choice)
+
+if user_choice == computer_choice:
+    print("It's a tie!")
+elif user_choice == "Rock" and computer_choice  == "Scissors":
+    print("Rock smashes scissors! You win!")
+elif user_choice == "Paper" and computer_choice  == "Rock":
+    print("Paper covers rock! You win!")
+elif user_choice == "Scissors" and computer_choice  == "Paper":
+    print("Scissors cuts paper! You win!")
+else:
+    print("You lose!.")
